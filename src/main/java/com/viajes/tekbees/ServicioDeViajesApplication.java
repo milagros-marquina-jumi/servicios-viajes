7639a3e7f2acfcb0cfbcfc87276d30f2f63a1f85
Milagros Marquina Mora <64057269+milagros-marquina-jumi@users.noreply.github.com>
2023-05-17 23:26:05 -0500

    estructura base

---
+++ src/main/java/com/viajes/tekbees/ServicioDeViajesApplication.java
@@ -0,0 +1,13 @@
+package com.viajes.tekbees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioDeViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioDeViajesApplication.class, args);
+	}
+
+}
