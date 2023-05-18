e3e136fa4e572f928f6a66b21feff7981e9f9f9e
Milagros Marquina Mora <64057269+milagros-marquina-jumi@users.noreply.github.com>
2023-05-17 23:26:05 -0500

    estructura base

---
+++ src/test/java/com/viajes/tekbees/ServicioDeViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viajes.tekbees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioDeViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
